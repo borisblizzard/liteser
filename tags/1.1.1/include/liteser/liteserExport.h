5e1e2140e1fa69668ad91c52a76a2389dcb825af
borisblizzard <borisblizzard@9d75fc61-6f2d-bf90-f154-7211f144b015>
2012-12-25 16:20:40 +0000

    - tagged v1.1.1

---
+++ tags/1.1.1/include/liteser/liteserExport.h
@@ -0,0 +1,47 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 1.1
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+/// 
+/// @section DESCRIPTION
+/// 
+/// Defines macros for DLL exports/imports.
+
+#ifndef LITESER_EXPORT_H
+#define LITESER_EXPORT_H
+
+	/// @def liteserExport
+	/// @brief Macro for DLL exports/imports.
+	/// @def liteserFnExport
+	/// @brief Macro for function DLL exports/imports.
+	#ifdef _STATICLIB
+		#define liteserExport
+		#define liteserFnExport
+	#else
+		#ifdef _WIN32
+			#ifdef LITESER_EXPORTS
+				#define liteserExport __declspec(dllexport)
+				#define liteserFnExport __declspec(dllexport)
+			#else
+				#define liteserExport __declspec(dllimport)
+				#define liteserFnExport __declspec(dllimport)
+			#endif
+		#else
+			#define liteserExport __attribute__ ((visibility("default")))
+			#define liteserFnExport
+		#endif
+	#endif
+	#ifndef DEPRECATED_ATTRIBUTE
+		#ifdef _MSC_VER
+			#define DEPRECATED_ATTRIBUTE __declspec(deprecated("function is deprecated"))
+		#else
+			#define DEPRECATED_ATTRIBUTE __attribute__((deprecated))
+		#endif
+	#endif
+
+#endif
+
---
+++ tags/1.1.1/src/Serializable.cpp
@@ -0,0 +1,48 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 1.1
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+
+#include <hltypes/hmap.h>
+#include <hltypes/hsbase.h>
+
+#include "liteser.h"
+#include "Serializable.h"
+
+namespace liteser
+{
+	Serializable::Serializable()
+	{
+	}
+
+	Serializable::~Serializable()
+	{
+	}
+
+	bool Serializable::serialize(hsbase* stream)
+	{
+		bool result = false;
+		unsigned int id = liteser::_lsIds.size() + 1; // necessary to avoid incorrect size() since ids[this] could be evaluated first
+		if (!liteser::_lsIds.has_value(this))
+		{
+			liteser::_lsIds[id] = this;
+			result = true;
+		}
+		else
+		{
+			id = liteser::_lsIds(this);
+		}
+		stream->dump(id);
+		return result;
+	}
+
+	bool Serializable::deserialize(hsbase* stream)
+	{
+		return true;
+	}
+		
+}
