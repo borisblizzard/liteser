483f3167aad25064c20d9cdc0e81349e116977b7
Boris Mikic <boris.blizzard@gmail.com>
2014-10-01 10:32:55 +0000

    - small update

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -42,11 +42,8 @@ namespace liteser
 		{
 			__lsConstructors = new hmap<hstr, Serializable* (*)()>();
 		}
-#if !defined (_IOS) && !defined(_MAC) // because of the order of global var initializations on iOS, mutexes and logTag get messed up
-		hlog::debug(liteser::logTag, "Registering class: " + name);
-#else
+		// because of the order of global var initializations, mutexes get messed up
 		printf("[liteser] Registering class: %s\n", name.c_str());
-#endif
 		__lsConstructors->operator[](name) = constructor;
 	}
 	
