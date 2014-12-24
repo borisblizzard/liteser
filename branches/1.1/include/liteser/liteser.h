8db4ca7d4da5d9aaf7aa09b1e64ed9d9bdaceb8b
boris.blizzard <boris.blizzard@9d75fc61-6f2d-bf90-f154-7211f144b015>
2014-12-24 12:43:38 +0000

    - hltypes update

--- branches/1.1/include/liteser/liteser.h
+++ branches/1.1/include/liteser/liteser.h
@@ -32,8 +32,8 @@
 	stream->dump((unsigned char)_LS_VERSION_MINOR);
 #define _LS_INIT_MANUAL_DESERIALIZATION \
 	liteser::_lsIds.clear(); \
-	unsigned char major = stream->load_uchar(); \
-	unsigned char minor = stream->load_uchar(); \
+	unsigned char major = stream->load_uint8(); \
+	unsigned char minor = stream->load_uint8(); \
 	liteser::checkVersion(major, minor);
 #define _LS_ASSIGN_HMAP_KEYS_VALUES(name) \
 	for_iter (_lsI ## name, 0, _lsKeys ## name.size()) \
@@ -71,13 +71,13 @@
 
 #define LS_SER(name) stream->dump(name);
 #define LS_SER_ENUM(name) stream->dump((int)name);
-/// @note "loadType" can be char, uchar, int, uint, long, ulong, short, ushort, bool, float, double or hstr
+/// @note "loadType" can be int8, uint8, int16, uint16, int32, uint32, int64, uint64, bool, float, double or hstr
 #define LS_DES(name, loadType) name = stream->load_ ## loadType();
-#define LS_DES_ENUM(name, type) name = (type)stream->load_int();
+#define LS_DES_ENUM(name, type) name = (type)stream->load_int32();
 
 #define LS_SER_OBJ(name) name.serialize(stream);
 #define LS_DES_OBJ(name) \
-	int _lsId ## name = stream->load_uint(); \
+	int _lsId ## name = stream->load_uint32(); \
 	liteser::_lsIds[_lsId ## name] = &name; \
 	name.deserialize(stream);
 
@@ -91,7 +91,7 @@
 		stream->dump(0); \
 	}
 #define LS_DES_OBJ_PTR(name, classe) \
-	int _lsId ## name = stream->load_uint(); \
+	int _lsId ## name = stream->load_uint32(); \
 	if (_lsId ## name != 0) \
 	{ \
 		if (liteser::_lsIds.has_key(_lsId ## name)) \
@@ -128,10 +128,10 @@
 	{ \
 		stream->dump((int)name[_lsI ## name]); \
 	}
-/// @note "type" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
-/// @note "loadType" can be char, uchar, int, uint, long, ulong, short, ushort, bool, float, double or hstr and has to correspond with "type"
+/// @note "type" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
+/// @note "loadType" can be int8, uint8, int16, uint16, int32, uint32, int64, uint64, bool, float, double or hstr and has to correspond with "type"
 #define LS_DES_ARRAY(name, type, loadType) \
-	int _lsCount ## name = stream->load_int(); \
+	int _lsCount ## name = stream->load_int32(); \
 	if (name != NULL) \
 	{ \
 		delete [] name; \
@@ -142,7 +142,7 @@
 		name[_lsI ## name] = stream->load_ ## loadType(); \
 	}
 #define LS_DES_ARRAY_ENUM(name, type, loadType) \
-	int _lsCount ## name = stream->load_int(); \
+	int _lsCount ## name = stream->load_int32(); \
 	if (name != NULL) \
 	{ \
 		delete [] name; \
@@ -150,7 +150,7 @@
 	} \
 	for_iter (_lsI ## name, 0, _lsCount ## name) \
 	{ \
-		name[_lsI ## name] = (type)stream->load_int(); \
+		name[_lsI ## name] = (type)stream->load_int32(); \
 	}
 
 #define LS_SER_ARRAY_OBJ(name, size) \
@@ -160,7 +160,7 @@
 		LS_SER_OBJ(name[_lsI ## name]); \
 	}
 #define LS_DES_ARRAY_OBJ(name, classe) \
-	int _lsCount ## name = stream->load_int(); \
+	int _lsCount ## name = stream->load_int32(); \
 	if (name != NULL) \
 	{ \
 		delete [] name; \
@@ -180,7 +180,7 @@
 		LS_SER_OBJ_PTR(name[_lsI ## name]); \
 	}
 #define LS_DES_ARRAY_OBJ_PTR(name, classe) \
-	int _lsCount ## name = stream->load_int(); \
+	int _lsCount ## name = stream->load_int32(); \
 	for_iter (_lsI ## name, 0, _lsCount) \
 	{ \
 		classe* _lsInstance = NULL; \
@@ -202,20 +202,20 @@
 	{ \
 		stream->dump((int)name[_lsI ## name]); \
 	}
-/// @note "type" can be char, uchar, int, uint, long, ulong, short, ushort, bool, float, double or hstr
+/// @note "type" can be int8, uint8, int16, uint16, int32, uint32, int64, uint64, bool, float, double or hstr
 #define LS_DES_HARRAY(name, type) \
 	name.clear(); \
-	int _lsCount ## name = stream->load_int(); \
+	int _lsCount ## name = stream->load_int32(); \
 	for_iter (_lsI ## name, 0, _lsCount ## name) \
 	{ \
 		name += stream->load_ ## type(); \
 	}
 #define LS_DES_HARRAY_ENUM(name, type) \
 	name.clear(); \
-	int _lsCount ## name = stream->load_int(); \
+	int _lsCount ## name = stream->load_int32(); \
 	for_iter (_lsI ## name, 0, _lsCount ## name) \
 	{ \
-		name += (type)stream->load_int(); \
+		name += (type)stream->load_int32(); \
 	}
 
 #define LS_SER_HARRAY_OBJ(name) \
@@ -225,7 +225,7 @@
 		LS_SER_OBJ(name[_lsI ## name]); \
 	}
 #define LS_DES_HARRAY_OBJ(name, classe) \
-	int _lsCount ## name = stream->load_int(); \
+	int _lsCount ## name = stream->load_int32(); \
 	for_iter (_lsI ## name, 0, _lsCount ## name) \
 	{ \
 		classe _lsInstance; \
@@ -240,7 +240,7 @@
 		LS_SER_OBJ_PTR(name[_lsI ## name]); \
 	}
 #define LS_DES_HARRAY_OBJ_PTR(name, classe) \
-	int _lsCount ## name = stream->load_int(); \
+	int _lsCount ## name = stream->load_int32(); \
 	for_iter (_lsI ## name, 0, _lsCount ## name) \
 	{ \
 		classe* _lsInstance = NULL; \
@@ -250,14 +250,14 @@
 
 // hmap serialization
 
-/// @note "keyType" and "valueType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
+/// @note "keyType" and "valueType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
 #define LS_SER_HMAP(name, keyType, valueType) \
 	harray<keyType> _lsKeys ## name = name.keys(); \
 	harray<valueType> _lsValues ## name = name.values(_lsKeys ## name); \
 	LS_SER_HARRAY(_lsKeys ## name); \
 	LS_SER_HARRAY(_lsValues ## name);
-/// @note "keyType" and "valueType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
-/// @note "keyLoadType" and "valueLoadType" can be char, uchar, int, uint, long, ulong, short, ushort, bool, float, double or hstr and have to correspond with "keyType" and "valueType"
+/// @note "keyType" and "valueType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
+/// @note "keyLoadType" and "valueLoadType" can be int8, uint8, int16, uint16, int32, uint32, int64, uint64, bool, float, double or hstr and have to correspond with "keyType" and "valueType"
 #define LS_DES_HMAP(name, keyType, keyLoadType, valueType, valueLoadType) \
 	name.clear(); \
 	harray<keyType> _lsKeys ## name; \
@@ -266,14 +266,14 @@
 	LS_DES_HARRAY(_lsValues ## name, valueLoadType); \
 	_LS_ASSIGN_HMAP_KEYS_VALUES(name);
 
-/// @note "keyType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
+/// @note "keyType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
 #define LS_SER_HMAP_V_OBJ(name, keyType, valueClass) \
 	harray<keyType> _lsKeys ## name = name.keys(); \
 	harray<valueClass> _lsValues ## name = name.values(_lsKeys ## name); \
 	LS_SER_HARRAY(_lsKeys ## name); \
 	LS_SER_HARRAY_OBJ(_lsValues ## name);
-/// @note "keyType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
-/// @note "keyLoadType" can be char, uchar, int, uint, long, ulong, short, ushort, bool, float, double or hstr and has to correspond with "keyType"
+/// @note "keyType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
+/// @note "keyLoadType" can be int8, uint8, int16, uint16, int32, uint32, int64, uint64, bool, float, double or hstr and has to correspond with "keyType"
 #define LS_DES_HMAP_V_OBJ(name, keyType, keyLoadType, valueClass) \
 	name.clear(); \
 	harray<keyType> _lsKeys ## name; \
@@ -282,14 +282,14 @@
 	LS_DES_HARRAY_OBJ(_lsValues ## name, valueClass); \
 	_LS_ASSIGN_HMAP_KEYS_VALUES(name);
 
-/// @note "keyType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
+/// @note "keyType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
 #define LS_SER_HMAP_V_OBJ_PTR(name, keyType, valueClass) \
 	harray<keyType> _lsKeys ## name = name.keys(); \
 	harray<valueClass> _lsValues ## name = name.values(_lsKeys ## name); \
 	LS_SER_HARRAY(_lsKeys ## name); \
 	LS_SER_HARRAY_OBJ_PTR(_lsValues ## name);
-/// @note "keyType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
-/// @note "keyLoadType" can be char, uchar, int, uint, long, ulong, short, ushort, bool, float, double or hstr and has to correspond with "keyType"
+/// @note "keyType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
+/// @note "keyLoadType" can be int8, uint8, int16, uint16, int32, uint32, int64, uint64, bool, float, double or hstr and has to correspond with "keyType"
 #define LS_DES_HMAP_V_OBJ_PTR(name, keyType, keyLoadType, valueClass) \
 	name.clear(); \
 	harray<keyType> _lsKeys ## name; \
@@ -298,14 +298,14 @@
 	LS_DES_HARRAY_OBJ_PTR(_lsValues ## name, valueClass); \
 	_LS_ASSIGN_HMAP_KEYS_VALUES(name);
 
-/// @note "valueType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
+/// @note "valueType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
 #define LS_SER_HMAP_K_OBJ(name, valueType, keyClass) \
 	harray<keyClass> _lsKeys ## name = name.keys(); \
 	harray<valueType> _lsValues ## name = name.values(_lsKeys ## name); \
 	LS_SER_HARRAY_OBJ(_lsKeys ## name); \
 	LS_SER_HARRAY(_lsValues ## name);
-/// @note "valueType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
-/// @note "valueLoadType" can be char, uchar, int, uint, long, ulong, short, ushort, bool, float, double or hstr and has to correspond with "valueType"
+/// @note "valueType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
+/// @note "valueLoadType" can be int8, uint8, int16, uint16, int32, uint32, int64, uint64, bool, float, double or hstr and has to correspond with "valueType"
 #define LS_DES_HMAP_K_OBJ(name, keyClass, valueType, valueLoadType) \
 	name.clear(); \
 	harray<keyClass> _lsKeys ## name; \
@@ -340,14 +340,14 @@
 	LS_DES_HARRAY_OBJ_PTR(_lsValues ## name, valueClass); \
 	_LS_ASSIGN_HMAP_KEYS_VALUES(name);
 
-/// @note "valueType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
+/// @note "valueType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
 #define LS_SER_HMAP_K_OBJ_PTR(name, valueType, keyClass) \
 	harray<keyClass*> _lsKeys ## name = name.keys(); \
 	harray<valueType> _lsValues ## name = name.values(_lsKeys ## name); \
 	LS_SER_HARRAY_OBJ_PTR(_lsKeys ## name); \
 	LS_SER_HARRAY(_lsValues ## name);
-/// @note "valueType" can be char, unsigned char, int, unsigned uint, long, unsigned long, short, unsigned short, bool, float, double or hstr
-/// @note "valueLoadType" can be char, uchar, int, uint, long, ulong, short, ushort, bool, float, double or hstr and has to correspond with "valueType"
+/// @note "valueType" can be char, unsigned char, short, unsigned short, int, unsigned uint, int64_t, uint64_t, bool, float, double or hstr
+/// @note "valueLoadType" can be int8, uint8, int16, uint16, int32, uint32, int64, uint64, bool, float, double or hstr and has to correspond with "valueType"
 #define LS_DES_HMAP_K_OBJ_PTR(name, keyClass, valueType, valueLoadType) \
 	name.clear(); \
 	harray<keyClass*> _lsKeys ## name; \
@@ -388,52 +388,6 @@ namespace liteser
 	liteserFnExport void deserialize(hsbase* stream, Serializable* object);
 	liteserFnExport void checkVersion(unsigned char major, unsigned char minor);
 
-	/*
-	/// @note Use only with integral types and hstr
-	template <class T>
-	void serialize(hsbase* stream, harray<T>& objects)
-	{
-		_LS_INIT_SERIALIZATION;
-		LS_SER_HARRAY(objects);
-	}
-	
-	/// @note Use only with integral types and hstr
-	template <class T>
-	void deserialize(hsbase* stream, harray<T>& objects)
-	{
-		_LS_INIT_DESERIALIZATION;
-		LS_DES_HARRAY(objects, T);
-	}
-	
-	template <class T>
-	void serialize_obj(hsbase* stream, harray<T>& objects)
-	{
-		_LS_INIT_SERIALIZATION;
-		LS_SER_HARRAY_OBJ(objects);
-	}
-	
-	template <class T>
-	void deserialize_obj(hsbase* stream, harray<T>& objects)
-	{
-		_LS_INIT_DESERIALIZATION;
-		LS_DES_HARRAY_OBJ(objects, T);
-	}
-	
-	template <class T>
-	void serialize_obj(hsbase* stream, harray<T*>& objects)
-	{
-		_LS_INIT_SERIALIZATION;
-		LS_SER_HARRAY_OBJ_PTR(objects);
-	}
-	
-	template <class T>
-	void deserialize_obj(hsbase* stream, harray<T*>& objects)
-	{
-		_LS_INIT_DESERIALIZATION;
-		LS_DES_HARRAY_OBJ_PTR(objects, T);
-	}
-	*/
-	
 	liteserExport extern hmap<unsigned int, Serializable*> _lsIds;
 
 }
